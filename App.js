9e3e024431e209762ec8d6d39dc179f645dd19fb
reyesrannie <117341670+reyesrannie@users.noreply.github.com>
2022-12-07 13:43:07 +0800

    styling

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 
 import WelcomeScreen from './app/screens/WelcomeScreen/WelcomeScreen';
 import VeiwingScreen from './app/screens/ViewingScreen/VeiwingScreen';
-
-import CardComponent from './app/components/CardComponent';
 import CardViewScreen from './app/screens/CardViewScreen/CardViewScreen';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen/ListingDetailsScreen';
 
 export default function App() {
   
   return (
-    <CardViewScreen />
+    <ListingDetailsScreen />
   );
 }
 
