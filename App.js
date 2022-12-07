6b4cedbaadb196a8fb9f4af27024a6fbe0b79d16
reyesrannie <117341670+reyesrannie@users.noreply.github.com>
2022-12-07 10:53:05 +0800

    Done Button

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import VeiwingScreen from './app/screens/VeiwingScreen';
+import AppButton from './app/components/AppButton';
+import styles from './app/components/styles';
 
 export default function App() {
+  
   return (
-    <VeiwingScreen/>
+    <WelcomeScreen />
   );
 }
+
 
 
