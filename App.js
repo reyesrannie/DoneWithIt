85df38cb82a05bf40ac392f28bbd729e8e193970
reyesrannie <117341670+reyesrannie@users.noreply.github.com>
2022-12-07 08:33:32 +0800

    Source

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import VeiwingScreen from './app/screens/VeiwingScreen';
 
 export default function App() {
   return (
-    <WelcomeScreen/>
+    <VeiwingScreen/>
   );
 }
 
