d20723bcbcea161f43662e988a2a9f7543fe5750
reyesrannie <117341670+reyesrannie@users.noreply.github.com>
2022-12-07 14:56:16 +0800

    List Item Sep

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import WelcomeScreen from './app/screens/WelcomeScreen/WelcomeScreen';
 import VeiwingScreen from './app/screens/ViewingScreen/VeiwingScreen';
 import CardViewScreen from './app/screens/CardViewScreen/CardViewScreen';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen/ListingDetailsScreen';
+import MessagesScreen from './app/screens/MessageScreen/MessagesScreen';
 
 export default function App() {
   
   return (
-    <ListingDetailsScreen />
+    <MessagesScreen />
   );
 }
 
