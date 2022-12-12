edcc22ee33cdbf4bae25ac83fd53973ede4dc925
reyesrannie <117341670+reyesrannie@users.noreply.github.com>
2022-12-12 09:14:02 +0800

    Touchable

--- App.js
+++ App.js
@@ -4,11 +4,13 @@ import VeiwingScreen from './app/screens/ViewingScreen/VeiwingScreen';
 import CardViewScreen from './app/screens/CardViewScreen/CardViewScreen';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessageScreen/MessagesScreen';
+import ListItemDeleteAction from './app/components/ListItemDeleteAction';
+import ListItem from './app/components/ListItem';
 
 export default function App() {
   
   return (
-    <MessagesScreen />
+    <ListItemDeleteAction />
   );
 }
 
