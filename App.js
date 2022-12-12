d1f78fff988a7c78dc3b3ab02eeb61514723ab31
reyesrannie <117341670+reyesrannie@users.noreply.github.com>
2022-12-12 15:02:38 +0800

    Picker

--- App.js
+++ App.js
@@ -12,16 +12,29 @@ import AreaScreenComponent from './app/components/AreaScreenComponent';
 import Icon from './app/components/Icon';
 import AccountScreen from './app/screens/AccountScreen/AccountScreen';
 import ListingScreen from './app/screens/ListingScreen/ListingScreen';
-import { Text, TextInput } from 'react-native';
+import { Switch, Text, TextInput } from 'react-native';
 import AppTextInput from './app/components/AppTextInput';
 import AppButton from './app/components/AppButton';
+import AppPicker from './app/components/AppPicker';
 
 export default function App() {
-  const [firstName, setFirstName] = useState('');
-  return (
-  <WelcomeScreen>
 
-  </WelcomeScreen>
+const categories = [
+{label: "Furniture", value: 1},
+{label: "Clothing", value: 2},
+{label: "Cameras", value: 3}
+]
+
+  const [isNew, setIsNew] = useState(false);
+const [category, setCategory] = useState(categories[0])
+  return (
+  <AreaScreenComponent>
+<AppPicker 
+selectedItem={category}
+onSelectItem = {item => setCategory(item)}
+items={categories} icon= "apps" placeholder="Category" />
+<AppTextInput icon="email" placeholder="email"/>
+  </AreaScreenComponent>
  
   );
 }
