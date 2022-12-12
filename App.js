6b3e0ad5ac9ddcf5dd93183be86e5520371b9afe
reyesrannie <117341670+reyesrannie@users.noreply.github.com>
2022-12-12 13:43:43 +0800

    style

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React, {useState} from 'react';
+
 
 import WelcomeScreen from './app/screens/WelcomeScreen/WelcomeScreen';
 import VeiwingScreen from './app/screens/ViewingScreen/VeiwingScreen';
@@ -10,11 +12,17 @@ import AreaScreenComponent from './app/components/AreaScreenComponent';
 import Icon from './app/components/Icon';
 import AccountScreen from './app/screens/AccountScreen/AccountScreen';
 import ListingScreen from './app/screens/ListingScreen/ListingScreen';
+import { Text, TextInput } from 'react-native';
+import AppTextInput from './app/components/AppTextInput';
+import AppButton from './app/components/AppButton';
 
 export default function App() {
-  
+  const [firstName, setFirstName] = useState('');
   return (
-  <ListingScreen />
+  <CardViewScreen>
+
+  </CardViewScreen>
+ 
   );
 }
 
