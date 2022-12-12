f328a3e8f820b77c7c1e4cd62bb0035b8ad24cd1
reyesrannie <117341670+reyesrannie@users.noreply.github.com>
2022-12-12 10:17:28 +0800

    icon

--- App.js
+++ App.js
@@ -6,11 +6,17 @@ import ListingDetailsScreen from './app/screens/ListingDetailsScreen/ListingDeta
 import MessagesScreen from './app/screens/MessageScreen/MessagesScreen';
 import ListItemDeleteAction from './app/components/ListItemDeleteAction';
 import ListItem from './app/components/ListItem';
+import AreaScreenComponent from './app/components/AreaScreenComponent';
+import Icon from './app/components/Icon';
 
 export default function App() {
   
   return (
-    <ListItemDeleteAction />
+    <AreaScreenComponent >
+    <ListItem title="My title"  ImageComponent={
+    <Icon 
+    name="email"/>}/>
+    </AreaScreenComponent>
   );
 }
 
