2410c0a912945e7a185701b28641772da4a04eb7
reyesrannie <117341670+reyesrannie@users.noreply.github.com>
2022-12-13 11:25:46 +0800

    DOne

--- App.js
+++ App.js
@@ -5,10 +5,11 @@ import RegisterScreen from './app/screens/RegisterScreen/RegisterScreen';
 import ListingDetailsScreen from './app/screens/ListingScreen/ListingDetailsScreen';
 import ListingEditScreen from './app/screens/ListingScreen/ListingEditScreen';
 import ListingScreen from './app/screens/ListingScreen/ListingScreen';
+import MessagesScreen from './app/screens/MessageScreen/MessagesScreen';
 export default function App() {
 
   return (
-  <ListingScreen />
+  <MessagesScreen />
   );
 }
 
