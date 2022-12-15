da348c5baf53eea16e80782cd013df7f691a5c6e
reyesrannie <117341670+reyesrannie@users.noreply.github.com>
2022-12-15 09:23:31 +0800

    Navigation

--- App.js
+++ App.js
@@ -1,17 +1,61 @@
-import React, {useEffect, useState} from 'react';
-import * as ImagePicker from 'expo-image-picker';
+import React, {} from 'react';
+import { Button, Text } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import AreaScreenComponent from './app/components/AreaScreenComponent';
-import { Button, Image } from 'react-native';
-import ImageImport from './app/components/ImageImport';
-import ImageImportList from './app/components/ImageImportList';
-import ListingEditScreen from './app/screens/ListingScreen/ListingEditScreen';
+import AuthNavigator from './app/navigation/AuthNavigator';
 
+
+const Link = () => {
+const navigation = useNavigation();
+
+
+return(
+  <Button 
+  title='Click'
+  onPress={()=> navigation.navigate('TweetDetails') }/>
+)};
+
+const Tweets = ({ navigation }) => (
+<AreaScreenComponent>
+<Text>Tweets</Text>
+<Link
+/>
+</AreaScreenComponent>
+);
+
+const TweetDetails = () => (
+<AreaScreenComponent>
+<Text>Tweet Details</Text>
+</AreaScreenComponent>
+);
+
+const Stack = createNativeStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets}/>
+    <Stack.Screen name="TweetDetails" component={TweetDetails}/>
+  </Stack.Navigator>
+)
+
+const Account = () => <AreaScreenComponent><Text>Account</Text></AreaScreenComponent>
+
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator screenOptions={{headerShown: false}}>
+  <Tab.Screen name="Feed" component={StackNavigator} />
+  <Tab.Screen name="Account" component={Account} />
+  </Tab.Navigator>
+)
 
 export default function App() {
 
   return (
- <ListingEditScreen />
+ <NavigationContainer>
+  <AuthNavigator />
+ </NavigationContainer>
   );
 }
 
