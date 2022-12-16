3c153b62a80a52b043ccdf6b5209db4e1310c93b
reyesrannie <117341670+reyesrannie@users.noreply.github.com>
2022-12-16 09:59:55 +0800

    WorkSpace

--- App.js
+++ App.js
@@ -1,63 +1,66 @@
-import React, {} from 'react';
-import { Button, Text } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from "react";
+import { Button, Text } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  NavigationContainer,
+  useNavigation,
+  DefaultTheme,
+} from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import AreaScreenComponent from './app/components/AreaScreenComponent';
-import AuthNavigator from './app/navigation/AuthNavigator';
-
+import navigationTheme from "./app/navigation/navigationTheme";
+import AreaScreenComponent from "./app/components/AreaScreenComponent";
+import AuthNavigator from "./app/navigation/AuthNavigator";
+import AppNavigator from "./app/navigation/AppNavigator";
+import FeedNavigator from "./app/navigation/FeedNavigator";
 
 const Link = () => {
-const navigation = useNavigation();
+  const navigation = useNavigation();
 
-
-return(
-  <Button 
-  title='Click'
-  onPress={()=> navigation.navigate('TweetDetails') }/>
-)};
+  return (
+    <Button title="Click" onPress={() => navigation.navigate("TweetDetails")} />
+  );
+};
 
 const Tweets = ({ navigation }) => (
-<AreaScreenComponent>
-<Text>Tweets</Text>
-<Link
-/>
-</AreaScreenComponent>
+  <AreaScreenComponent>
+    <Text>Tweets</Text>
+    <Link />
+  </AreaScreenComponent>
 );
 
 const TweetDetails = () => (
-<AreaScreenComponent>
-<Text>Tweet Details</Text>
-</AreaScreenComponent>
+  <AreaScreenComponent>
+    <Text>Tweet Details</Text>
+  </AreaScreenComponent>
 );
 
 const Stack = createNativeStackNavigator();
 const StackNavigator = () => (
   <Stack.Navigator>
-    <Stack.Screen name="Tweets" component={Tweets}/>
-    <Stack.Screen name="TweetDetails" component={TweetDetails}/>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
   </Stack.Navigator>
-)
+);
 
-const Account = () => <AreaScreenComponent><Text>Account</Text></AreaScreenComponent>
+const Account = () => (
+  <AreaScreenComponent>
+    <Text>Account</Text>
+  </AreaScreenComponent>
+);
 
 const Tab = createBottomTabNavigator();
 const TabNavigator = () => (
-  <Tab.Navigator screenOptions={{headerShown: false}}>
-  <Tab.Screen name="Feed" component={StackNavigator} />
-  <Tab.Screen name="Account" component={Account} />
+  <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Screen name="Feed" component={StackNavigator} />
+    <Tab.Screen name="Account" component={Account} />
   </Tab.Navigator>
-)
+);
 
 export default function App() {
-
   return (
- <NavigationContainer>
-  <AuthNavigator />
- </NavigationContainer>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
-
-
-
